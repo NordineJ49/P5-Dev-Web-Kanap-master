d56dd7823686d0eb674a9d1edeb747c058469679
NordineJ49 <nordine.j49@gmail.com>
2023-01-05 10:56:37 +0100

    .

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -2,7 +2,9 @@
  * Fonction qui permet d'insérer l'id de la commande dans le DOM
  */
 const id = () => {
+    // récupération du paramètre 'orderId' dans l'url 
     const newId = new URLSearchParams(window.location.search).get('orderId');
+    //récupération de la balise qui a pour id 'orderId' puis on y insère en textContent newId
     document.getElementById('orderId').textContent = newId
 }
 id()
